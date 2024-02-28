947942e46efa255ce4d3e657ad364d3baea69d00
ahmeda313 <syedahmeda313@gmail.com>
2024-02-28 20:07:38 +0530

    about2.2

--- src/components/About2.jsx
+++ src/components/About2.jsx
@@ -5,23 +5,18 @@ import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 
 const About2 = ()=> {
   return (
-    // <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 sm:py-24 lg:py-32">
-    <div className="bg-white">
-    <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-    <div className="relative isolate overflow-hidden animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-
-
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-10 p-10">  
+    <div className="relative isolate overflow-hidden animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500">
+      <div className="mx-auto max-w-7xl py-10">
+            <div className="mx-10 ">  
                     <Date day="14" className="" month="March" subtxt="th"/>
 
-                    <h1 className="text-4xl md:text-6xl  text-center mt-10 text-white">
+                    <h1 className="text-3xl md:text-6xl  text-center mt-10 ">
                     JBIET Main block
 
                     </h1>
             
             <br />
-                <h1 className="text-center mt-10 text-white text-6xl lg:text-8xl">
+                <h1 className="text-center mt-10  text-4xl md:text-6xl">
                     It's Show time
                 </h1>
             </div>
@@ -38,7 +33,7 @@ const About2 = ()=> {
                 </radialGradient>
               </defs>
             </svg> */}
-      <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
+      {/* <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
         <div
           className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
           style={{
@@ -46,9 +41,7 @@ const About2 = ()=> {
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
         />
-      </div>
-    </div>
-    </div>
+      </div> */}
     </div>
     </div>
 
--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -3,9 +3,9 @@ function Date(props){
     return (
         <div className=" text-center">
             <h1 className="relative text-9xl">
-                {props.day} <span className=" text-4xl absolute lg:-ms-10 -ms-4">{props.subtxt}</span>
-            <span className=" text-5xl">{props.month}</span>
+                {props.day} <span className=" text-4xl absolute  -ms-4">{props.subtxt}</span>
             </h1>
+            <span className=" text-5xl">{props.month}</span>
          </div>
 )
 }
