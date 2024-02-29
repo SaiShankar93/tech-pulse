73c8342c0cb52abfb4a6be8dabcc12cd682ef3bf
ahmeda313 <syedahmeda313@gmail.com>
2024-02-29 21:32:19 +0530

    overflow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,13 +9,9 @@ const Card = ({ imageSrc, title, description }) => {
 
   return (
     <div className="max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow-lg transition-all duration-700 hover:shadow-blue-700 hover:scale-101 dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
         <img className="rounded-t-lg" src={imageSrc} alt="" />
-      </a>
       <div className="p-5">
-        <a href="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-        </a>
         <p className="mb-3 h-12 font-normal text-gray-700 dark:text-gray-400">{description}</p>
         <button onClick={toggleOverlay} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           More
@@ -27,17 +23,22 @@ const Card = ({ imageSrc, title, description }) => {
       
       {showOverlay && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 overflow-hidden" onClick={toggleOverlay}>
-          <div className="bg-white p-3 rounded-lg w-full lg:w-1/2 ">
-            <div className="flex justify-end">
+          <div className="bg-white px-10 py-5 rounded-lg w-full lg:w-1/2 ">
+            <div className="flex justify-between text-2xl mb-5">
+            <h1>Overlay Content</h1>
             <button onClick={toggleOverlay} ><svg viewBox="0 -0.5 8 8" className="h-5" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>close_mini [#1522]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-385.000000, -206.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <polygon id="close_mini-[#1522]" points="334.6 49.5 337 51.6 335.4 53 333 50.9 330.6 53 329 51.6 331.4 49.5 329 47.4 330.6 46 333 48.1 335.4 46 337 47.4"> </polygon> </g> </g> </g> </g></svg></button>
             </div>
-            <div className="content p-4">
-            <h1>Overlay Content</h1>
-            <p className='text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sequi quibusdam magnam in voluptatibus assumenda repudiandae dolores nisi suscipit provident quidem corporis nemo animi nam rem eius debitis, illum fuga! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere reiciendis repellat eius delectus fuga, commodi unde illo sunt. Vel neque dicta sapiente magnam hic architecto illum voluptatem qui aliquid natus.</p>
+            <div className="content p-5  mb-5 overflow-scroll max-h-64 lg:overflow-x-hidden">
+
+            <p className='text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sequi quibusdam magnam in voluptatibus assumenda repudiandae dolores nisi suscipit provident quidem corporis nemo animi nam rem eius debitis, illum fuga! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere reiciendis repellat eius delectus fuga, commodi unde illo sunt. Vel neque dicta sapiente magnam hic architecto illum voluptatem qui aliquid natus.</p>
             </div>
-            <button class="bg-transparent text-xs hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            Register now!
-          </button>
+            <div className="flex justify-end">
+              <a href="#"
+                className="rounded-md text-xs bg-indigo-600 px-3.5 py-2.5  font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Register now!
+              </a>
+              </div>
           </div>
         </div>
       )}
