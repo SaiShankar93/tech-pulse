1d19a9de926b33ce6c8752808127ed683b579b02
Sai Shankar <saishankar15052005@gmail.com>
2024-03-01 02:17:59 +0530

    add .

--- src/components/About2.jsx
+++ src/components/About2.jsx
@@ -1,31 +1,12 @@
 import  '../styles/App.css';
 import Date from './Date';
-import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 
-
-const About2 = ()=> {
-  return (
-    // <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 sm:py-24 lg:py-32">
-    <div className="bg-white">
-    <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-    <div className="relative isolate overflow-hidden bg-white px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-
-
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-10 p-10">  
-                    <Date day="14" className="" month="March" subtxt="th"/>
-
-                    <h1 className="text-4xl md:text-6xl  text-center mt-10 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent">
-                    JBIET Main block
-
-                    </h1>
-            
-            <br />
-                <h1 className="text-center mt-10 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-6xl lg:text-8xl">
-                    It's Show time
-                </h1>
-            </div>
-            {/* <svg
+const About2 = () => {
+    return (
+        <div className="bg-white" id="details">
+        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 ">
+          <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-10 w-full sm:rounded-3xl sm:px-16  lg:flex lg:gap-x-20 lg:px-24" id="glow">
+            <svg
               viewBox="0 0 1024 1024"
               className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
               aria-hidden="true"
@@ -33,27 +14,29 @@ const About2 = ()=> {
               <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
               <defs>
                 <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                  <stop stopColor="#7775D6" />
                   <stop offset={1} stopColor="#E935C1" />
                 </radialGradient>
               </defs>
-            </svg> */}
-      <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
-        <div
-          className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
-    </div>
-    </div>
-    </div>
-    </div>
-
-  )
+            </svg>
+            <div className="mx-auto">
+                <div className="mx-auto text-center">
+                    <Date day="14" month="March" subtxt="th" />
+                </div>
+                {/* 🗓️ */}
+                <div className="mx-auto text-center">
+                    <h1 className="text-white venue">📍JBIET, Main block</h1>
+                </div>
+            <br />
+            <div className="timer_container text-center">
+                <h1 className="title">
+                    It's Show<span>time</span>
+                </h1>
+            </div>
+        
+        </div>
+        </div> </div>
+        </div>
+    );
 }
-
 
 export default About2;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,38 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import '../styles/sass.scss';
 
-const Card = ({ imageSrc, title, description }) => {
-  const [showOverlay, setShowOverlay] = useState(false);
-
+const Card = ({ imageSrc, title, description, rules,id }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef(null);
   const toggleOverlay = () => {
-    setShowOverlay(!showOverlay);
+    setIsOpen(!isOpen);
+  }
+  useEffect(() => {
+    document.getElementById('modal-container').classList.add('out');
+    document.body.classList.remove('modal-active');
+  }, [isOpen]);
+
+  const handleModalContainerClick = (e) => {
+    if (!modalRef.current.contains(e.target)) {
+      document.getElementById('modal-container').classList.add('out');
+      document.body.classList.remove('modal-active');
+    }
+  };
+
+  const handleButtonClick = (e) => {
+    const buttonId = e.currentTarget.id;
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.removeAttribute('class');
+    modalContainer.classList.add(buttonId);
+    document.body.classList.add('modal-active');
   };
 
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img className="rounded-t-lg" src={imageSrc} alt="" />
-      </a>
-      <div className="p-5">
+    <>
+    
+      <div className="max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow-lg transition-all duration-700 hover:shadow-blue-700 hover:scale-101 dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          <img className="rounded-t-lg" src={imageSrc} alt="" />
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        <button onClick={toggleOverlay} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Read more
-          <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-          </svg>
-        </button>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          </a>
+          <p className="mb-3 h-12 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+          <button id='one' onClick={handleButtonClick} className="button inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Know rules
+            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </button>
+        </div>
       </div>
-      {showOverlay && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-lg">
-            <h1>Overlay Content</h1>
-            <button onClick={toggleOverlay}>Close</button>
+
+
+      <div id="modal-container" onClick={handleModalContainerClick}>
+        <div className="modal-background">
+          <div ref={modalRef} className={`modal ${isOpen ? '' : ''}`}>
+            <h2>{title}</h2>
+            <p>{rules}</p>
+            <button onClick={toggleOverlay}>close</button>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
