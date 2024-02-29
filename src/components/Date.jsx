946776af61e844618a3c1acef47be2d507b86ef9
ahmeda313 <syedahmeda313@gmail.com>
2024-02-29 20:40:09 +0530

    headline animation

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -2,7 +2,7 @@ import '../styles/Date.css';
 function Date(props){
     return (
         <div className="mx-auto text-center">
-            <h1 className="date_title">
+            <h1 className="date_title ">
                 {props.day} <sup>{props.subtxt}</sup>
             <span className="month_title">{props.month}</span>
             </h1>
