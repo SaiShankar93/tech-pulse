97628e19b0b751ee9a59347047bf41563e99b450
Sai Shankar <113768646+SaiShankar93@users.noreply.github.com>
2024-03-04 23:00:42 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -11,7 +11,8 @@ const cardData = [
     imageSrc: BB,
     title: 'BUGBASH  (12:00 PM)',
     description: 'Gear Up for the Bug Bash: Debugging Marathon in Python, Java, and C!',
-    rules: `Bug Bash technical event would typically involve students from computer science or related fields coming together to identify and fix bugs in the given code. During the event, participant would work collaboratively to review code, run tests, and debug issues to improve the quality of the code. This hands-on experience allows students to apply their knowledge in a practical setting, gain valuable problem-solving skills, and learn about software development best practices. Overall, Bug Bashes provide students with an opportunity to sharpen their technical skills, work in teams, and contribute to real-world projects.
+    rules: ` Registration Fee: RS.50
+    Bug Bash technical event would typically involve students from computer science or related fields coming together to identify and fix bugs in the given code. During the event, participant would work collaboratively to review code, run tests, and debug issues to improve the quality of the code. This hands-on experience allows students to apply their knowledge in a practical setting, gain valuable problem-solving skills, and learn about software development best practices. Overall, Bug Bashes provide students with an opportunity to sharpen their technical skills, work in teams, and contribute to real-world projects.
 
     RULES:
     1.Solo participants race against time to debug software.
