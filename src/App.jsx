3a994204053b3baaf2073e0b4c6096bef06b4367
Sai Shankar <113768646+SaiShankar93@users.noreply.github.com>
2024-03-06 11:32:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import About2 from "./components/About2";
 import Events from "./components/Events";
 import Intro from "./components/Intro";
-import Sponsor from "./components/sponsor"
+import Sponsor from "./components/Sponsor";
+import Register from "./components/register"; // Make sure you have this component created
+
 function App() {
   const [showIntro, setShowIntro] = useState(true);
 
@@ -13,25 +16,31 @@ function App() {
     const timer = setTimeout(() => {
       setShowIntro(false);
     }, 3000);
-
     return () => clearTimeout(timer);
   }, []); 
 
   return (
-    <>
+    <Router>
       {showIntro ? (
         <Intro />
       ) : (
         <>
           <Navbar />
-          <About />
-          <About2 />
-          <Events />
-          <Sponsor/>
-          <Footer />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <About />
+                <About2 />
+                <Events />
+                <Sponsor />
+                <Footer />
+              </>
+            }/>
+            <Route path="/register" element={<Register />} />
+          </Routes>
         </>
       )}
-    </>
+    </Router>
   );
 }
 
