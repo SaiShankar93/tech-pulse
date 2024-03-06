d47d198a712b7d8b805a81cfbc5c38c1347cf4c6
Sai Shankar <113768646+SaiShankar93@users.noreply.github.com>
2024-03-06 11:48:59 +0530

    Update dialog.jsx

--- src/components/dialog.jsx
+++ src/components/dialog.jsx
@@ -107,7 +107,7 @@ function Dialog({ eventid, title, rules,close }) {
                         close
                       </button>
                       <a
-                        href="https://forms.gle/qv8uKMkvRiefqZP17" target="_blank"
+                        href="https://saishankar93.github.io/Payment-gateway/" target="_blank"
                         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       >
                         Register now!
